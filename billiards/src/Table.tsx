1b724defee311c428a72cf6b51e7a9154b7c5d93
wodeni <wn2155@columbia.edu>
2023-11-08 16:17:52 -0500

    feat: basic video player interactivity

--- billiards/src/Table.tsx
+++ billiards/src/Table.tsx
@@ -8,13 +8,16 @@ import {
   interp,
   lt,
   mul,
+  opaque,
   select,
   sqrt,
   sub,
   vec,
 } from "rose";
+import { createSignal } from "solid-js";
 import { createMutable } from "solid-js/store";
 
+// constants
 const maxSteps = 2048;
 const visInterval = 64;
 const outputInterval = 16;
@@ -27,7 +30,6 @@ const goal = [0.9, 0.75];
 const radius = 0.03;
 const elasticity = 0.8;
 
-const dt = 0.003;
 // const alpha = 0.0;
 const learningRate = 0.01;
 
@@ -35,68 +37,14 @@ const init_x: number[] = [];
 const init_v: number[] = [];
 const loss: number[] = [];
 
+const [dt, setdt] = createSignal(0.003);
+
 // all the balls
 const alpha = 0;
-let x: number[][] = createMutable([[0.1, 0.5]]);
-let v: number[][] = createMutable([
-  [0.3 * Math.cos(alpha), 0.3 * Math.sin(alpha)],
-]);
+let x: number[][] = createMutable([]);
+let v: number[][] = createMutable([]);
 
-for (let i = 0; i < 10; i++) {
-  v.push([0, 0]);
-}
-
-// function scan(zoom: number): void {
-//   const N = 1000;
-//   const angles: number[] = [];
-//   const losses: number[] = [];
-//   // forward(true, "initial");
-
-//   for (let i = 0; i < N; i++) {
-//     const alpha = ((i + 0.5) / N - 0.5) * Math.PI * zoom;
-//     init_x[0] = 0.1;
-//     init_x[1] = 0.5;
-//     init_v[0] = 0.3 * Math.cos(alpha);
-//     init_v[1] = 0.3 * Math.sin(alpha);
-
-//     loss[0] = 0;
-//     // clear();
-//     forward(false);
-//     console.log(loss[0]);
-
-//     losses.push(loss[0]);
-//     angles.push(Math.degrees(alpha));
-//   }
-// }
-
-// function optimize(): void {
-//   init_x[0] = 0.1;
-//   init_x[1] = 0.5;
-//   init_v[0] = 0.3;
-//   init_v[1] = 0.0;
-
-//   // forward(visualize=true, output='initial');
-
-//   for (let iter = 0; iter < 200; iter++) {
-//     ti.root.dense(ti.l, 64).place(loss);
-//     if (iter % 20 == 19) {
-//       output = `iter${iter.toString().padStart(4, "0")}`;
-//     } else {
-//       output = undefined;
-//     }
-//     forward(true, output);
-
-//     console.log(`Iter=${iter} Loss=${loss[0]}`);
-//     for (let d = 0; d < 2; d++) {
-//       init_x[d] -= learning_rate * init_x.grad[d];
-//       init_v[d] -= learning_rate * init_v.grad[d];
-//     }
-//   }
-
-//   clear();
-//   forward(true, "final");
-// }
-
+// vector helpers
 const Vec2 = Vec(2, Real);
 
 const vadd2 = fn([Vec2, Vec2], Vec2, (v1, v2) =>
@@ -124,11 +72,18 @@ const normalize = fn([Vec2, Real], Vec2, (v, eps) =>
   vmul(div(1, add(eps, norm(v))), v)
 );
 
+const print = opaque([Real], Real, (x) => {
+  console.log(x);
+  return x;
+});
+
+// ball collision
+
 const collidePair = fn(
   [Vec2, Vec2, Vec2, Vec2],
   { x_inc_contrib: Vec2, imp: Vec2 },
   (x1, x2, v1, v2) => {
-    const dist = vsub2(vadd2(x1, vmul(dt, v1)), vadd2(x2, vmul(dt, v2)));
+    const dist = vsub2(vadd2(x1, vmul(dt(), v1)), vadd2(x2, vmul(dt(), v2)));
     const distNorm = norm(dist);
     const relativeV = vsub2(v1, v2);
     const dir = normalize(dist, 1e-6);
@@ -137,69 +92,63 @@ const collidePair = fn(
     const imp = select(
       cond,
       Vec2,
-      vmul(mul(-(1 + elasticity) * 0.5, projectedV), dir),
+      vmul(mul(-(1 + elasticity) * 0.5 * 1, projectedV), dir),
       [0, 0]
     );
     const toi = div(sub(distNorm, 2 * radius), min(-1e-3, projectedV));
     const x_inc_contrib = select(
       cond,
       Vec2,
-      vmul(min(sub(toi, dt), 0), imp),
+      vmul(min(sub(toi, dt()), 0), imp),
       [0, 0]
     );
     return { x_inc_contrib, imp };
   }
 );
 
-const play = () => {
-  setInterval(() => {
-    // collide all pairs of balls
-    const x_inc: number[][] = [];
-    const impulse: number[][] = [];
-    for (let i = 0; i < ballCount; i++) {
-      x_inc.push([0, 0]);
-    }
-    for (let i = 0; i < ballCount; i++) {
-      impulse.push([0, 0]);
-    }
-    for (let i = 0; i < ballCount; i++) {
-      for (let j = 0; j < ballCount; j++) {
-        if (i === j) continue;
-
-        // console.log(x, v, x[i], x[j], v[i], v[j]);
-
-        const { x_inc_contrib, imp } = interp(collidePair)(
-          x[i],
-          x[j],
-          v[i],
-          v[j]
-        );
-        x_inc[j] = [
-          x_inc[j][0] + x_inc_contrib[0],
-          x_inc[j][1] + x_inc_contrib[1],
-        ];
-        impulse[j] = [impulse[j][0] + imp[0], impulse[j][1] + imp[1]];
-      }
-    }
-
-    // update ball positions
-    for (let i = 0; i < ballCount; i++) {
-      v[i] = [v[i][0] + impulse[i][0], v[i][1] + impulse[i][1]];
-      x[i] = [
-        x[i][0] + x_inc[i][0] + dt * v[i][0],
-        x[i][1] + x_inc[i][1] + dt * v[i][1],
+// main simulation step
+const step = () => {
+  // collide all pairs of balls
+  const x_inc: number[][] = [];
+  const impulse: number[][] = [];
+  for (let i = 0; i < ballCount; i++) {
+    x_inc.push([0, 0]);
+  }
+  for (let i = 0; i < ballCount; i++) {
+    impulse.push([0, 0]);
+  }
+  for (let i = 0; i < ballCount; i++) {
+    for (let j = 0; j < ballCount; j++) {
+      if (i === j) continue;
+      const { x_inc_contrib, imp } = interp(collidePair)(
+        x[i],
+        x[j],
+        v[i],
+        v[j]
+      );
+      x_inc[i] = [
+        x_inc[i][0] + x_inc_contrib[0],
+        x_inc[i][1] + x_inc_contrib[1],
       ];
+      impulse[i] = [impulse[i][0] + imp[0], impulse[i][1] + imp[1]];
     }
-  }, 5);
+  }
+
+  // update ball positions
+  for (let i = 0; i < ballCount; i++) {
+    v[i] = [v[i][0] + impulse[i][0], v[i][1] + impulse[i][1]];
+    x[i] = [
+      x[i][0] + x_inc[i][0] + dt() * v[i][0],
+      x[i][1] + x_inc[i][1] + dt() * v[i][1],
+    ];
+  }
 };
 
-// const advance =
-
-export default function Table() {
-  const [w, h] = [1024, 1024];
-  const pixelRadius = Math.round(radius * 1024) + 1;
-  const cloth = "#2bb4e5";
-
+const init = () => {
+  // initialize cue ball position
+  x[0] = [0.1, 0.5];
+  v[0] = [0.3 * Math.cos(alpha), 0.3 * Math.sin(alpha)];
+  // initialize other balls
   let count = 0;
   for (let i = 0; i < layers; i++) {
     for (let j = 0; j < i + 1; j++) {
@@ -208,8 +157,20 @@ export default function Table() {
         i * 2 * radius + 0.5,
         j * 2 * radius + 0.5 - i * radius * 0.7,
       ];
+      v[count] = [0, 0];
     }
   }
+};
+
+export default function Table() {
+  const [w, h] = [1024, 1024];
+  const pixelRadius = Math.round(radius * 1024) + 1;
+  const cloth = "#2bb4e5";
+  const [playID, setPlayID] = createSignal(0);
+  const [playing, setPlaying] = createSignal(false);
+  const fps = 60;
+
+  init();
 
   return (
     <>
@@ -230,7 +191,34 @@ export default function Table() {
           fill="#000"
         ></circle>
       </svg>
-      <button onclick={() => play()}>Play</button>
+      <button
+        onclick={() => {
+          if (playing()) {
+            clearInterval(playID());
+            setPlaying(false);
+            return;
+          } else {
+            setPlaying(true);
+            setPlayID(setInterval(() => step(), 1000 / fps));
+          }
+        }}
+      >
+        {playing() ? `Pause` : `Play`}
+      </button>
+      <button onclick={() => init()}>Reset</button>
+      <div>
+        Time step:
+        <input
+          type="range"
+          min={0.001}
+          max={0.05}
+          step={0.0001}
+          value={dt()}
+          // disabled={playing()}
+          onchange={(v) => setdt(v.target.valueAsNumber)}
+          name="Time step"
+        />
+      </div>
     </>
   );
 }
