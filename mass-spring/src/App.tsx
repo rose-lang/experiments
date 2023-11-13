d7fd264eaad1cfdce6133e7045963f1e3f29a968
wodeni <wn2155@columbia.edu>
2023-11-13 14:32:55 -0500

    chore: initial commit

---
+++ mass-spring/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Table from "./Table";
+
+function App() {
+  return (
+    <>
+      <h1>Billiards</h1>
+      <p>Differentiable pool physics with 🌹</p>
+      <div class="table">
+        <Table />
+      </div>
+    </>
+  );
+}
+
+export default App;
