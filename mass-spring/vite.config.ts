a98707182751d30d2e8536cda0d7faf51123e113
Sam Estep <sam@samestep.com>
2023-11-15 15:19:23 -0500

    Revert "Mass spring example (#1)"
    This reverts commit b349e1a0135efb1e3fd8ebc8aa778dacfeb39821.

--- mass-spring/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
-
-import topLevelAwait from "vite-plugin-top-level-await";
-
-export default defineConfig({
-  plugins: [solid(), topLevelAwait()],
-  optimizeDeps: { exclude: ["rose"] },
-});
